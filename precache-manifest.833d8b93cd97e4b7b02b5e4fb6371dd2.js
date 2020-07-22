f6d97080d7ec1f6dc48574acf7605c87bf83d444
Hendriyan Ogivano <hogivano@gmail.com>
2020-07-22 18:53:16 +0700

    Updates

---
+++ precache-manifest.833d8b93cd97e4b7b02b5e4fb6371dd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4676a60895f783b63e4283bf4f0abe31",
+    "url": "/ycp-test/index.html"
+  },
+  {
+    "revision": "e3a1dd249087f253fa28",
+    "url": "/ycp-test/static/css/main.d1eb2391.chunk.css"
+  },
+  {
+    "revision": "ffeb019c59b5d92891bb",
+    "url": "/ycp-test/static/js/2.335ab379.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ycp-test/static/js/2.335ab379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a1dd249087f253fa28",
+    "url": "/ycp-test/static/js/main.d1fe767e.chunk.js"
+  },
+  {
+    "revision": "ea61bc3bb99b4f55b59e",
+    "url": "/ycp-test/static/js/runtime-main.027eb2d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ycp-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
